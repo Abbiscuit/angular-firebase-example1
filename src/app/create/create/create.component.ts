d2ffe29b973d587a88c2ed636f5e4ab9fce83da0
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-04-08 22:11:32 +0900

    feat(create): ペット選択カルーセル追加

--- src/app/create/create/create.component.ts
+++ src/app/create/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-create',
@@ -7,11 +8,23 @@ import { FormBuilder, Validators, FormControl } from '@angular/forms';
   styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  petIds = [...Array(10)].map((_, i) => i + 1);
+  config: SwiperConfigInterface = {
+    loop: true,
+    navigation: true,
+    pagination: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+  };
+
+  selectedPetId = 0;
+
   form = this.fb.group({
     name: ['', [Validators.required, Validators.maxLength(40)]],
     gender: ['', [Validators.required, Validators.pattern(/male|female/)]],
   });
 
+  // error-handling
   get nameControl() {
     return this.form.get('name') as FormControl;
   }
@@ -22,5 +35,6 @@ export class CreateComponent implements OnInit {
 
   submit() {
     console.log(this.form.value);
+    console.log(this.selectedPetId);
   }
 }
